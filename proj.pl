ebd7806ba2295c71aac3791dca25c8714a498ce7
Guilherme <up202007375@up.pt>
2024-04-04 10:48:25 +0100

    refactor
    Co-authored-by: JozeRamos <JozeRamos@users.noreply.github.com>

--- proj.pl
+++ proj.pl
@@ -6,16 +6,21 @@ color_mazes(Maze, N):-
     length(Colors, NumColors),
     all_equal(Colors),
     fill(Colors, 0, NumColors),
-    MazeSize is N * N,
     Path = [],
     Start #= N * (N - 1),
     Finish #= N - 1,
-    find_path(Maze, Colors, Start, Finish, Path, N).
+    find_path(Maze, Colors, Start, Finish, Path, N),
+    % STATISTICS
+    fd_statistics,
+    statistics(total_runtime,[_,T]),
+    TS is ((T//10)*10)/1000,
+    write('Time: '), write(TS), write('s').
+
 
 fill([], _, 0).
-fill([X|Xs], X, N) :- N > 0, N0 is N - 1,fill(Xs, X, N0).
+fill([X|Xs], X, N) :- N > 0, N0 #= N - 1, fill(Xs, X, N0).
 
-find_path(_, _, _, F, P, _) :- last(P, F), write(P), nl.
+find_path(_, C, _, F, P, _) :- last(P, F), all_equal(C), write(C), nl, write(P), nl, nl.
 find_path(Maze, Colors, Position, Finish, Path, N) :-
     \+ member(Position, Path),  % Ensure Position is not already in the path
     append(Path, [Position], P), % Add Position to the path
@@ -24,7 +29,7 @@ find_path(Maze, Colors, Position, Finish, Path, N) :-
     findall(X, (
         member(X, AdjacentList),
         find_path(Maze, NewColors, X, Finish, P, N)
-        ), PathsList). % Recursively call find_path for each adjacent position
+        ), _). % Recursively call find_path for each adjacent position
 
 replace(I, L, E, K) :-
   nth0(I, L, _, R),
@@ -34,42 +39,37 @@ update_colors(Maze, Colors, Position, NewColors):-
     nth0(Position, Maze, ColorValue),
     (
         ColorValue =:= 0 -> NewColors = Colors;
-        ColorPosition is ColorValue - 1,
+        ColorPosition #= ColorValue - 1,
         nth0(ColorPosition, Colors, ColorCount),
-        NewColorCount is ColorCount + 1,
+        NewColorCount #= ColorCount + 1,
         replace(ColorPosition, Colors, NewColorCount, NewColors)
     ).
 
 adjacent_positions(Position, N, Adjacent):-
-    Up is Position - N,
-    Down is Position + N,
-    Left is Position - 1,
-    Right is Position + 1,
+    Up #= Position - N,
+    Down #= Position + N,
+    Left #= Position - 1,
+    Right #= Position + 1,
     All = [Up, Down, Left, Right],
     findall(X, (member(X, All), is_valid_position(X, Position, N)), Adjacent).
 
 is_valid_position(X, P, N):-
     X >= 0,
     X < N * N,
-    X_Mod is X mod N,
-    P_Mod is P mod N,
-    Diff is abs(X_Mod - P_Mod),
+    X_Mod #= X mod N,
+    P_Mod #= P mod N,
+    Diff #= abs(X_Mod - P_Mod),
     Diff #< 2.
 
-%     X-N > 0
-% X-1  X  X+1
-%     X+N < N*N-1
+% EXAMPLES
 
-% N = 4
-% X = 3
-
-% N mod N = 0
-% X mod N = 3
-
-% 2 verificações
-
-% color_mazes([1,2,2,0,2,3,3,3,1,2,2,2,0,0,1,2],3).
+% color_mazes([1,2,2,0,2,3,3,3,1,2,2,2,0,0,1,2],4).
+% SOLUTION: [12,8,9,13,14,10,6,7,3]
 % 1,2,2,0,
 % 2,3,3,3,
 % 1,2,2,2,
 % 0,0,1,2
+
+% color_mazes([1,1,2,2,0,2,2,3,3,4,1,0,0,2,4,4,2,4,2,2,0,3,3,0,1],5).
+% 1 - blue, 2 - red, 3 - yellow, 4 - green  
+% SOLUTION: [20,15,10,11,6,1,2,7,8,9,4]
