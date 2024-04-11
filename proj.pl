ba8fe2fa1df7d5ebdb8438a602544535ee4c68af
Guilherme <up202007375@up.pt>
2024-04-11 16:40:52 +0100

    path with subcircuit

--- proj.pl
+++ proj.pl
@@ -15,15 +15,9 @@ solve_maze :-
 
     % Model the Path
     length(Path, Size),
-    element(1, Path, Start),
-    element(Size, Path, Finish),
-
-    % Model edges
-    path_constraints(Path, Maze, N),
-
-    % Ensure that the path represents a subcircuit.
-    % !Removed because it breaks the code
-    % subcircuit(Path), 
+    domain(Path, 1, Size),
+    % element(Finish, Path, Start),
+    path_constraints(Path, Path, N),
 
     % TODO: Model Colors
     % maximum(NumColors, Maze),
@@ -31,39 +25,61 @@ solve_maze :-
     % calculate_colors(Path, Maze, Colors),
     % all_equal(Colors),
 
+    % Ensure that the path represents a subcircuit.
+    subcircuit(Path),
+
     % Find a solution.
     labeling([], Path),
 
     % Print the solution.
-    write(Path), nl.
+    % write('Path: '), write(Path), nl.
+    % write('Start: '), write(Start), nl,
+    % write('Finish: '), write(Finish), nl.
+
+    print_path(Start, Path, Finish).
+    % fd_statistics.
 
 % ------------------------------------------------
 % Path as a List Constraints
-path_constraints([_], _, _).
-path_constraints([Current, Next | Rest], _, N) :-
-    Current #= N, Next #= Current,
-    path_constraints([Next | Rest], _, N).
+path_constraints([], _, _).
+path_constraints([Current | Rest], Path, N) :-
+    element(Position, Path, Current),
+    neighbor(Position, Current, N),
+    path_constraints(Rest, Path, N).
 
-path_constraints([Current, Next | Rest], Maze, N) :-
-    neighbor(Current, Next, Maze, N),
-    path_constraints([Next | Rest], Maze, N).
+% Close the path
+neighbor(Finish, Start, N) :-
+    Finish #= N, Start #= N * N - (N - 1).
 
-neighbor(Current, Next, Maze, N) :-
-    Size is N * N,
-    domain([Current, Next], 1, Size),
-    Diff #= abs((Current-1) mod N - (Next-1) mod N),
-    Current #= Next + 1 #/\ Diff #= 1 #\/ %v Right
-    Current #= Next - 1 #/\ Diff #= 1 #\/ %^ Left
-    Current #= Next + N #/\ Current mod N #\= 0 #\/ %> Down
-    Current #= Next - N . %< Up
+% Check if two nodes are neighbors
+neighbor(Position, Neighbor, N) :-
+    Diff #= abs((Position-1) mod N - (Neighbor-1) mod N),
+    Position #= Neighbor - N #\/ % Up
+    % Position #= Neighbor + N #\/ % Down
+    Position #= Neighbor + N #/\ Position mod N #\= 0 #\/ % Down with optimization
+    Position #= Neighbor + 1 #/\ Diff #= 1 #\/ % Right
+    Position #= Neighbor - 1 #/\ Diff #= 1 #\/ % Left
+    Position #= Neighbor. % Self
 
 % ------------------------------------------------
-% TODO: Model Colors
+% TODO Model Colors
 % calculate_colors([], _, _).
 % calculate_colors([Current | Rest], Maze, Colors) :-
 %     element(Current, Maze, ColorValue),
-%     ColorValue #= 0 #\/ 
+%     ColorValue #= 0 #\/
 %     element(ColorValue, Colors, ColorCount),
 %     NewColorCount #= ColorCount + 1,
 %     element(ColorValue, Colors, NewColorCount),
 %     calculate_colors(Rest, Maze, Colors).
+
+% ------------------------------------------------
+% TODO Print the path.
+print_path(Finish, _, Finish):- write(Finish), nl.
+print_path(Position, Path, Finish) :-
+    element(Position, Path, Next),
+    (
+        Position \= Finish ->
+        write(Position),
+        write(' -> ')
+    ),
+    print_path(Next, Path, Finish).
