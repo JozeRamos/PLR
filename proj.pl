7d254d8272cd78bd13e01f56c5275bbb44d22bda
Guilherme <up202007375@up.pt>
2024-05-16 11:20:14 +0100

    refactor

--- proj.pl
+++ proj.pl
@@ -26,9 +26,11 @@ solve_maze :-
   % Find a possible solution
   labeling([], Path),
 
-  % TODO Model the Colors
+  % Maze with relevant colors
   length(NewMaze, Size),
   color_constraint(Maze, Path, NewMaze, 1),
+
+  % Count colors
   maximum(NumColors, Maze),
   length(Colors, NumColors),
   countAll(NewMaze, NumColors, Colors),
@@ -60,43 +62,32 @@ neighbor(Position, Neighbor, N) :-
 
 color_constraint(_, [], [], _).
 color_constraint(Maze, [H|T], [H1|T1], N) :-
-    N #= H,
-    H1 is 0,
-    N1 is N + 1,
-    color_constraint(Maze, T, T1, N1),!.    
+  N #= H,
+  H1 #= 0,
+  N1 #= N + 1,
+  color_constraint(Maze, T, T1, N1),!.    
 color_constraint(Maze, [H|T], [H1|T1], N) :-
-    element(H, Maze, H1),
-    N1 is N + 1,
-    color_constraint(Maze, T, T1, N1).
+  element(H, Maze, H1),
+  N1 #= N + 1,
+  color_constraint(Maze, T, T1, N1).
 
 % count maze colors
 countAll(List, N, Colors) :-
-    length(L, N),
-    domain(L, 1, N),
-    all_distinct(L),
-    labeling([], L),
-    count_min(List, Colors, L), !.
+  length(L, N),
+  domain(L, 1, N),
+  all_distinct(L),
+  labeling([], L),
+  count_min(List, Colors, L), !.
 
 count_min(_, [], []).
 count_min(List, [H1|T1], [H|T]) :-
-    count(H, List, #=, H1),
-    count_min(List, T1, T).
+  count(H, List, #=, H1),
+  count_min(List, T1, T).
 
 % ------------------------------------------------
-% Auxiliar Predicates
-filter(Path, Position) :-
-  element(Position, Path, Position).
-
-filter_maze(Maze, FilteredMaze, Position) :-
-  element(Position, Maze, FilteredMaze).
-
-% TODO findall of each value in the list, and length of the result to get the count
-count_(List, Value, Count) :-
-  aggregate_all(count, member(Value, List), Count).
-
-write_path(_, _, Finish, Finish) :- write(Finish), !.
-write_path(Path, Maze, Position, Finish) :-
-  element(Position, Maze, Color),
-  write(Position), write(' ('), write(Color), write(') -> '),
-  element(Position, Path, Next),
-  write_path(Path, Maze, Next, Finish).
+% write_path(_, _, Finish, Finish) :- write(Finish), !.
+% write_path(Path, Maze, Position, Finish) :-
+%   element(Position, Maze, Color),
+%   write(Position), write(' ('), write(Color), write(') -> '),
+%   element(Position, Path, Next),
+%   write_path(Path, Maze, Next, Finish).
